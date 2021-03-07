043d774fbabec271c4a6a71fb253817e17230a90
Karl Matti <28657926+karlmatti@users.noreply.github.com>
2021-03-07 19:16:00 +0200

    set is_running to see if the led is running

--- python/server.py
+++ python/server.py
@@ -1,8 +1,21 @@
-import json
+import led, microphone, visualization
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+global is_running
+
+
+def change_reaction(reaction):
+    pass
+
+
+def start():
+    is_running = True
+    # Initialize LEDs
+    led.update()
+    # Start listening to live audio stream
+    microphone.start_stream(visualization.microphone_update)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,10 +24,18 @@ def hello():
         return render_template('form.html')
 
     if request.method == 'POST':
-        # form_data = request.form
         reaction = request.form['reaction']
-        return "You chose " + reaction
+        available_reactions = ['scroll', 'energy', 'spectrum']
+        if reaction in available_reactions:
+
+            if not is_running:
+                start()
+            change_reaction(reaction)
+            return "You chose " + reaction + ", is_running=" + is_running
+        else:
+            return "This kind of selection is not allowed!"
 
 
 if __name__ == '__main__':
+    is_running = False
     app.run(host='0.0.0.0')
