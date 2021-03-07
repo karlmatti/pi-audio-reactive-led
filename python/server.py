4379e379a2fe9d72caceaac5e40226d79fa1b19c
Karl Matti <28657926+karlmatti@users.noreply.github.com>
2021-03-07 18:48:23 +0200

    dump request result

--- python/server.py
+++ python/server.py
@@ -12,7 +12,7 @@ def hello():
 
     if request.method == 'POST':
         # form_data = request.form
-        reaction = request.form.reaction
+        reaction = json.dumps(request.form.reaction)
         return "You chose " + reaction
 
 
